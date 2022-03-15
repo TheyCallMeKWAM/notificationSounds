7c3250ec56bde5b891a69ac8afb9e19fe3fd051b
William Sampson <54544795+TheyCallMeKWAM@users.noreply.github.com>
2022-03-15 13:31:33 -0400

    First Upload

---
+++ Sound Effect Website/index.js
@@ -0,0 +1,21 @@
+var audio = new Audio("audiofiles/discord.mp3");
+
+audio.oncanplaythrough = function(){
+audio.play();
+}
+
+var audio2 = new Audio("audiofiles/googlemeet.mp3");
+
+audio2.oncanplaythrough = function(){
+audio2.play();
+}
+var audio3 = new Audio("audiofiles/teams.mp3");
+
+audio3.oncanplaythrough = function(){
+audio3.play();
+}
+
+var audio4 = new Audio("audiofiles/slack.mp3");
+audio4.oncanplaythrough = function(){
+audio4.play();
+}
